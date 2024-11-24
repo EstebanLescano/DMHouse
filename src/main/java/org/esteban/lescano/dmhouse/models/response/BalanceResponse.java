fb69aedd77eb835e1bf173cd6ff65c78ddc5d52d
Esteban <esteban.lescano@bitlogic.io>
2024-11-24 08:47:38 -0300

    creacion de endpoint saldo y billetera

--- src/main/java/org/esteban/lescano/dmhouse/models/response/BalanceResponse.java
+++ src/main/java/org/esteban/lescano/dmhouse/models/response/BalanceResponse.java
@@ -3,6 +3,6 @@ package org.esteban.lescano.dmhouse.models.response;
 import java.math.BigDecimal;
 
 public class BalanceResponse {
-	public String money;
-	public BigDecimal balance;
+	public static String money;
+	public static BigDecimal balance;
 }
