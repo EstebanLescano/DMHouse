87449f6247cc8c10d56401680aed38865ab658a3
Esteban <esteban.lescano@bitlogic.io>
2024-12-01 08:34:00 -0300

    config de swagger para que se muestre en el puerto

--- src/main/java/org/esteban/lescano/dmhouse/security/SecurityConfig.java
+++ src/main/java/org/esteban/lescano/dmhouse/security/SecurityConfig.java
@@ -28,10 +28,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf
+                .ignoringRequestMatchers("/swagger-ui/**", "/v3/api-docs/**")
+        );
         http.authorizeHttpRequests(auth -> auth
+                // Permitir acceso a Swagger y OpenAPI
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                // Configurar rutas específicas
                 .requestMatchers(HttpMethod.GET, "/wallet/**").permitAll()
                 .requestMatchers("/admin/**").hasRole(ADMIN)
                 .requestMatchers("/").hasRole(USER)
+                // Proteger el resto de las rutas
                 .anyRequest().authenticated()
         );
         http.oauth2ResourceServer(oauth2 -> oauth2
