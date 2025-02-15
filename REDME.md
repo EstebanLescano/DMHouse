636f63333d7dd1ad3b3838b398b259bc5675c561
Esteban <esteban.lescano@bitlogic.io>
2025-02-15 09:10:35 -0300

    Revert "integracion de keycloak funcionando"
    This reverts commit 1540fa915e852cd7dc67e41b042fa3354269701f.

--- REDME.md
+++ REDME.md
@@ -1,27 +1,19 @@
-🚀 WEB API Billetera Virtual: 
-Sistema de transferencia de dinero electrónico con el que se pueden hacer múltiples operaciones financieras.
+🚀 WEB API Billetera Virtual: Sistema de transferencia de dinero electrónico con el que se pueden hacer múltiples operaciones financieras.
 
 🚀 Framework: Spring Boot.
 
-🚀 Base de Datos: relacional Postgres
-nombre de DB dmh en el archivo console.sql del proyecto esta la base con sus relaciones 
-🔧 Lenguaje: Java 21.
+🚀 Base de Datos: relacional MYSQL.
 
 🔧 Testing: Tests Unitarios.
 
-⚙️ Servicios externos: Keycloak.
-
-⚙️ Servicios externos: Eureka. para descubrir los servicios y para balanceo de cargas.
-http://localhost:8761/ 
+⚙️ Servicios adicionales: Se utilizó el servicio de MAILGUN para poder notificar con mails transaccionales los movimientos relizados en la billetera.
 
 ⚙️ Seguridad: Se utilizó JWT Token para seguridad.
 
 🔧 Otros: JPA, Hibernate, Maven.
 
-📋 Prueba API: Se utilizó Swagger para crear la documentacion de las apis. 
-http://localhost:8500/swagger-ui/index.html#/User/getUser
+📋 Prueba API: Se utilizó Postman para probar la WEB API.
 
 ⌨️ GIT: Se trabajaron ramas y se probaron Pull Requests.
 
-📦 Deploy: Heroku (url: https://mibilleteravirtual.herokuapp.com). 
-Se utilizó Postgre para base de datos, con la finalidad de consumir el servicio de manera gratuita.
+📦 Deploy: Heroku (url: https://mibilleteravirtual.herokuapp.com). Se utilizó Postgre para base de datos, con la finalidad de consumir el servicio de manera gratuita.
--- user-service/src/main/java/org/esteban/userservice/controller/UserController.java
+++ user-service/src/main/java/org/esteban/userservice/controller/UserController.java
@@ -3,14 +3,14 @@ package org.esteban.userservice.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.esteban.userservice.entity.Person;
-import org.esteban.userservice.models.ClientDTO;
-import org.esteban.userservice.models.PersonDTO;
-import org.esteban.userservice.service.ClientService;
 import org.esteban.userservice.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
@@ -19,16 +19,13 @@ import java.util.Optional;
 @Tag(name = "User", description = "Manage user-related operations")
 public class UserController {
 
-    private final PersonService personService;
-    private final ClientService clientService;
+    @Autowired
+    private PersonService personService;
 
-    public UserController(PersonService personService, ClientService clientService) {
-        this.personService = personService;
-        this.clientService = clientService;
+    public UserController() {
     }
 
-
-    @GetMapping("/person/{id}")
+    @GetMapping("/users/{id}")
     @Operation(summary = "Get User by ID", description = "Retrieve user details based on the provided user ID")
     public ResponseEntity<Person> getUser(@PathVariable("id") Integer id) {
         Optional<Person> user = personService.getPersonById(id);
@@ -36,14 +33,4 @@ public class UserController {
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
-    @PostMapping("/person")
-    @Operation(summary = "Create User", description = "Create a new user")
-    public ResponseEntity<String> createUser(@RequestBody ClientDTO clientDTO) {
-        try {
-            personService.createPerson(clientService.registerUser(clientDTO));
-            return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating user");
-        }
-    }
 }
