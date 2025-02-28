3ef069956e067b544dc4f768088ed1af43d535c1
Esteban Lescano <estebanles22@gmail.com>
2025-02-28 16:08:40 -0300

    creacion de user

--- user-service/src/main/java/org/esteban/userservice/exceptions/ClientAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.esteban.userservice.exceptions;
-
-public class ClientAlreadyExistsException extends RuntimeException {
-    public ClientAlreadyExistsException(String message) {
-        super(message);
-    }
-}
